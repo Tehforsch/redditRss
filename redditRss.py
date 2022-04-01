02e413c7bda8148cc7ae176a1293a802b84ddfac
tehforsch <mail@tonipeter.de>
2022-04-01 14:07:51 +0200

    adjust self post bonus

--- redditRss.py
+++ redditRss.py
@@ -3,6 +3,8 @@ import datetime
 import time
 from feedgen.feed import FeedGenerator
 
+SELF_POST_BONUS = 150
+
 def readSecrets():
     with open("redditAppData", "r") as f:
         lines = [l.rstrip("\n") for l in f.readlines()]
@@ -18,13 +20,13 @@ reddit = praw.Reddit(**readSecrets())
 def readTopPosts(subredditName, numPosts, minUpvotes, filters):
     submissions = [submission for submission in list(reddit.subreddit(subredditName).top(limit=numPosts*10, time_filter="day"))]
     if filters != []:
-        submissions = [submission for submission in submissions if not any(flairToFilter.lower() in submission.link_flair_text.lower() for flairToFilter in filters)]
+        submissions = [submission for submission in submissions if (submission.link_flair_text is None) or not any(flairToFilter.lower() in submission.link_flair_text.lower() for flairToFilter in filters)]
     submissions = [sub for sub in submissions if getScore(sub) >= minUpvotes]
     return submissions[:numPosts]
 
 def getScore(submission):
     if submission.is_self:
-        return submission.score * 20
+        return submission.score * SELF_POST_BONUS
     else:
         return submission.score
 
@@ -55,10 +57,10 @@ def getDescription(submission):
         description = description + "<img src=\"{url}\"><br><a href=\"{url}\">".format(url=submission.url)
     return description
 
-def generateSubredditRss(subredditName, numPosts, minUpvotes, filters):
+def generateSubredditRss(subredditName, fname, numPosts, minUpvotes, filters):
     generateRss(
             submissions=readTopPosts(subredditName, numPosts, minUpvotes, filters),
-            outputFile="feeds/{}.xml".format(subredditName),
+            outputFile="feeds/{}.xml".format(fname),
             title="{}".format(subredditName),
             description="Top posts {}".format(subredditName),
             link="reddit.com/r/{}/top".format(subredditName),
@@ -85,10 +87,10 @@ def generateMultiSubredditRss(subreddits):
 def getSubredditList():
     with open("subreddits", "r") as f:
         for l in f.readlines():
-            name, numPosts, minUpvotes, *filters = l.replace("\n", "").split()
+            name, fname, numPosts, minUpvotes, *filters = l.replace("\n", "").split()
             numPosts = int(numPosts)
             minUpvotes = int(minUpvotes)
-            yield (name, numPosts, minUpvotes, filters)
+            yield (name, fname, numPosts, minUpvotes, filters)
 
-for (subreddit, numPosts, minUpvotes, *filters) in getSubredditList():
-    generateSubredditRss(subreddit, numPosts, minUpvotes, *filters)
+for (subreddit, fname, numPosts, minUpvotes, *filters) in getSubredditList():
+    generateSubredditRss(subreddit, fname, numPosts, minUpvotes, *filters)
